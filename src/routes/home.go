214f4fb5a3348853a01d176ffdef060ef5500590
Sanjay Suthar <sanjaymsuthar@gmail.com>
2022-04-22 11:02:38 +0530

    minor

--- src/routes/home.go
+++ src/routes/home.go
@@ -2,11 +2,12 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func HomePage(c *gin.Context) {
 
-	c.JSON(200, gin.H{
+	c.JSON(http.StatusOK, gin.H{
 		"message": "Welcome to Account Management System.",
 		//"data":    o,
 	})
